9a51f10aa30114952e7520e760e4d705fc60f12d
Jesse Sandvik <sandvikjesse@gmail.com>
2021-11-14 17:02:53 -0500

    refactored scss styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Dashboard from '../views/dashboard';
+import UserProfile from '../views/profile';
 
 const HomePage = () => {
     const { isAuthenticated, user } = useAuth0();
 
-    return (
-        <div>
-            {isAuthenticated && (
-                <Dashboard user={user} />
-            )}
-        </div>
-    );
+    if (isAuthenticated) {
+        return (
+            <>
+                <div className="item item1">
+                    <UserProfile user={user} />
+                </div>
+                <div className="item item2">
+                    <Dashboard />
+                </div>
+            </>
+        );
+    } else {
+        return <h2>Welcome to Froggy!</h2>
+    }
 }
 
 export default HomePage;
--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import UserProfile from './profile';
 
-const Dashboard = props => {
+const Dashboard = () => {
     return (
         <div>
-            <UserProfile user={props.user}/>
+            <h2>Dashboard</h2>
         </div>
     );
 }
