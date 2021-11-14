babb516421ff074a12182868904558cb44099eac
Jesse Sandvik <sandvikjesse@gmail.com>
2021-11-14 17:09:08 -0500

    added frog logo to header

--- src/components/siteTitle.js
+++ src/components/siteTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SiteTitle = () => {
     return (
         <div className="siteTitle">
-            <h1>Froggy.</h1>
+            <h1><i className="fas fa-frog"></i>Froggy.</h1>
             <small>Those bugs never stood a chance.</small>
         </div>
     );
