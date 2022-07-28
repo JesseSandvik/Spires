3172a46602b636fa42e89721b2d0826293c35825
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-28 01:11:15 -0400

    further styling and layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Button from "./components/atoms/button/Button";
 import Footer from "./components/organisms/footer/Footer";
 import Header from "./components/organisms/header/Header";
 import Heading from "./components/atoms/heading/Heading";
+import Icon from "./components/atoms/icon/Icon";
 import Main from "./components/organisms/main/Main";
 
 import "./css/styles.css";
@@ -11,7 +12,10 @@ function App() {
     <div className="app">
       <Header>
         <div>
-          <Heading level="1">spires</Heading>
+          <Button onClick={() => console.log("click")}>
+            <Heading level="1">spires</Heading>
+            <Icon className="fa-solid fa-grip-lines-vertical" />
+          </Button>
         </div>
         <div>
           <Button onClick={() => console.log("click")}>login</Button>
