fa371e268e28e9d9d3355eb979c9d43bca123377
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-28 00:39:58 -0400

    layout and basic styling established

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Button from "./components/atoms/button/Button";
+import Footer from "./components/organisms/footer/Footer";
+import Header from "./components/organisms/header/Header";
+import Heading from "./components/atoms/heading/Heading";
+import Main from "./components/organisms/main/Main";
+
+import "./css/styles.css";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="app">
+      <Header>
+        <div>
+          <Heading level="1">spires</Heading>
+        </div>
+        <div>
+          <Button onClick={() => console.log("click")}>login</Button>
+          <Button onClick={() => console.log("click")}>sign up</Button>
+        </div>
+      </Header>
+      <Main />
+      <Footer>&copy; jessesandvik {new Date().getFullYear()}</Footer>
+    </div>
+  );
 }
 
 export default App;
