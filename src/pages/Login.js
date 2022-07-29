b4216ab4ba58dfd03d97795cdb272492413dcc0d
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-28 23:02:42 -0400

    updated styling for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,24 @@ import { useNavigate } from "react-router-dom";
 
 import Button from "../components/atoms/button/Button";
 import Heading from "../components/atoms/heading/Heading";
-import Main from "../components/organisms/main/Main";
 
 function Login() {
   const navigate = useNavigate();
 
   return (
-    <Main id="login">
+    <section id="login">
       <div>
         <Heading level="2">Log in to Spires</Heading>
-        <Button onClick={() => navigate("/dashboard")}>
-          continue as guest
-        </Button>
-        <Button onClick={() => console.log("click")}>
-          continue with username
-        </Button>
+        <div>
+          <Button onClick={() => navigate("/dashboard")}>
+            continue as guest
+          </Button>
+          <Button onClick={() => console.log("click")}>
+            continue with username
+          </Button>
+        </div>
       </div>
-    </Main>
+    </section>
   );
 }
 
