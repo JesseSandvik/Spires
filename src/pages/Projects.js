48bf48b823f4944c489b02ded931c1e71e0a4761
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-07 13:45:38 -0400

    updated layout and styling for projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,8 +3,13 @@ import Heading from "../components/atoms/heading/Heading";
 function Projects() {
   return (
     <section id="projects">
+      <Heading level="2">projects</Heading>
       <div>
-        <Heading level="2">projects</Heading>
+        <div>
+          <Heading level="5">
+            sample project name with a brief description
+          </Heading>
+        </div>
       </div>
     </section>
   );
