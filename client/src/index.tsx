fb6dd8d90725f017ea367c1818477361462a7188
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-04 17:49:07 -0400

    auth0 initialized client side, established ENV variables for Auth0 config, flow working

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const AUTH0_DOMAIN: any = process.env.REACT_APP_AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID: any = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-fqf8o-g3.us.auth0.com"
-      clientId="Stt0mKi9isn4IO4Mp2aPTrXaUjahZqAd"
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
     >
       <App />
